9ddc47442a65cb0941bdb7b0d3ff83761a115ad8
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-15 11:59:04 +0100

    form register created

---
+++ client/src/Signup.jsx
@@ -0,0 +1,54 @@
+export default function Signup() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Register</h2>
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter name"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Register
+          </button>
+          <p>Already have an Account</p>
+          <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
